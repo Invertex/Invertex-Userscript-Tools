a65b22033c79ea7cc4e13c3395f24c2f8c8fba1d
Dabbles in too many things <invertexyz@proton.me>
2024-10-09 16:54:23 -0700

    Update userscript_tools.js

--- userscript_tools.js
+++ userscript_tools.js
@@ -1,7 +1,3 @@
-
-const dlSVG = '<g><path d="M 8 51 C 5 54 5 48 5 42 L 5 -40 C 5 -45 -5 -45 -5 -40 V 42 C -5 48 -5 54 -8 51 L -48 15 C -51 12 -61 17 -56 22 L -12 61 C 0 71 0 71 12 61 L 56 22 C 61 17 52 11 48 15 Z"></path>' +
-    '<path d="M 56 -58 C 62 -58 62 -68 56 -68 H -56 C -62 -68 -62 -58 -56 -58 Z"></path></g>';
-
 //Greasemonkey does not have this functionality, so helpful way to check which function to use
 const isGM = (typeof GM_addValueChangeListener === 'undefined');
 
